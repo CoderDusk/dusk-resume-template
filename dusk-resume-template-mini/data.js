66ec817032746d28cfb147d4d2394f1108db1de6
coderdusk <coderdusk@qq.com>
2021-10-02 12:31:31 +0800

    新增 静态迷你版
    新增加形态迷你版，下载后编辑data.js修改信息即可使用

---
+++ dusk-resume-template-mini/data.js
@@ -0,0 +1,50 @@
+let data = {
+  "basicInfo": {
+    "fullName": "姓名",
+    "job": "应聘岗位",
+    "birthDate": "出生年月",
+    "email": "电子邮箱",
+    "mobile": "手机号码",
+    "wechat": "微信号",
+    "salary": "期望薪资",
+    "address": "目前地点/期望地点"
+  },
+  "selfIntroduction": "前端开发者，能独立完成桌面端、移动端前端开发，了解后端开发（可以使用Serverless技术开发后端），熟悉项目整个周期流程。",
+  "educationExperience": [{
+    "schoolName": "学校名",
+    "degree": "程度",
+    "major": "专业",
+    "duration": "学习时长"
+  }, {
+    "schoolName": "学校名",
+    "degree": "本科",
+    "major": "计算机网络技术",
+    "duration": "2011-2015"
+  }],
+  "workExperience": [{
+    "companyName": "公司名称1",
+    "job": "职位1",
+    "duration": "2020.03-2021.02",
+    "content": "工作内容\n\n22222\n\n换行"
+  }, {
+    "companyName": "公司名称2",
+    "job": "职位2",
+    "duration": "工作时长",
+    "content": "工作内容"
+  }],
+  "techStack": [{
+    "name": "MVVM框架",
+    "list": "Vue,React"
+  }],
+  "resumeDownloadLink": "https://abc.com/xx.pdf",
+  "projectExperience": [{
+    "name": "项目名称",
+    "content": "项目经历描述",
+    "link": "https://www.baidu.com/",
+    "pictureList": ["https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-tpxtzduzbujqb9a12e/5c2822c4-e330-4fad-98ae-da88bc717c87.png", "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-tpxtzduzbujqb9a12e/b803ec89-524a-4f68-91b1-b53beef77823.png"],
+    "QRCode": "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-tpxtzduzbujqb9a12e/12cf6090-57c0-11eb-8a36-ebb87efcf8c0.png"
+  }],
+  "title": "XXX - 前端开发 - 简历",
+  "siteURL": "http://hello.com/abc",
+  "isResumeData": "yes"
+}
